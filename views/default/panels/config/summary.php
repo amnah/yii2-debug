f0e1d3c8e7c952e9ff07a6dd500c72b006471609
amnah <amnah.dev@gmail.com>
2016-05-29 14:25:53 -0400

    make toolbar more compact and add number of included files

--- views/default/panels/config/summary.php
+++ views/default/panels/config/summary.php
@@ -4,7 +4,6 @@
 <div class="yii-debug-toolbar__block">
     <a href="<?= $panel->getUrl() ?>">
         <span class="yii-debug-toolbar__label"><?= $panel->data['application']['yii'] ?></span>
-        PHP
-        <span class="yii-debug-toolbar__label"><?= $panel->data['php']['version'] ?></span>
+        <span class="yii-debug-toolbar__label"><?= substr($panel->data['php']['version'], 0, 10) ?></span>
     </a>
 </div>
--- views/default/panels/profile/summary.php
+++ views/default/panels/profile/summary.php
@@ -4,6 +4,7 @@
 /* @var $memory integer */
 ?>
 <div class="yii-debug-toolbar__block">
-    <a href="<?= $panel->getUrl() ?>" title="Total request processing time was <?= $time ?>">Time <span class="yii-debug-toolbar__label yii-debug-toolbar__label_info"><?= $time ?></span></a>
-    <a href="<?= $panel->getUrl() ?>" title="Peak memory consumption">Memory <span class="yii-debug-toolbar__label yii-debug-toolbar__label_info"><?= $memory ?></span></a>
+    <a href="<?= $panel->getUrl() ?>" title="Total request processing time was <?= $time ?>"><span class="yii-debug-toolbar__label yii-debug-toolbar__label_info"><?= $time ?></span></a>
+    <a href="<?= $panel->getUrl() ?>" title="Peak memory consumption"><span class="yii-debug-toolbar__label yii-debug-toolbar__label_info"><?= $memory ?></span></a>
+    <a href="<?= $panel->getUrl() ?>" title="Included files"><span class="yii-debug-toolbar__label yii-debug-toolbar__label_info"><?= count(get_included_files()) ?> files</span></a>
 </div>

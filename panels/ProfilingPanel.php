38062b8c392c7e3537546a6c69fdfaf46656663f
amnah <amnah.dev@gmail.com>
2016-06-08 10:39:41 -0400

    fix missing functions from ProfilingPanel

--- panels/ProfilingPanel.php
+++ panels/ProfilingPanel.php
@@ -2,8 +2,42 @@
 
 namespace amnah\yii2\debug\panels;
 
+use Yii;
+use yii\debug\models\search\Profile;
+
 class ProfilingPanel extends \yii\debug\panels\ProfilingPanel
 {
+    /**
+     * @inheritdoc
+     */
+    public function getSummary()
+    {
+        return Yii::$app->view->render('panels/profile/summary', [
+            'memory' => sprintf('%.3f MB', $this->data['memory'] / 1048576),
+            'time' => number_format($this->data['time'] * 1000) . ' ms',
+            'numFiles' => !empty($this->data["numFiles"]) ? "{$this->data["numFiles"]} files" : "",
+            'panel' => $this
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getDetail()
+    {
+        $searchModel = new Profile();
+        $dataProvider = $searchModel->search(Yii::$app->request->getQueryParams(), $this->getModels());
+
+        return Yii::$app->view->render('panels/profile/detail', [
+            'panel' => $this,
+            'dataProvider' => $dataProvider,
+            'searchModel' => $searchModel,
+            'memory' => sprintf('%.3f MB', $this->data['memory'] / 1048576),
+            'time' => number_format($this->data['time'] * 1000) . ' ms',
+            'numFiles' => !empty($this->data["numFiles"]) ? "{$this->data["numFiles"]} files" : "",
+        ]);
+    }
+
     /**
      * @inheritdoc
      */

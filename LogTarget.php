4b7f276ace2c6bdac4d3c0a916ea8bcd7f7ea860
amnah <amnah.dev@gmail.com>
2016-06-08 10:22:51 -0400

    fix numFiles inconsistency between toolbar and index data

--- LogTarget.php
+++ LogTarget.php
@@ -13,10 +13,18 @@ class LogTarget extends \yii\debug\LogTarget
         $summary = parent::collectSummary();
 
         // add in profiling data
-        $data = $this->module->panels['profiling']->save();
-        $summary['profilingTime'] = $data['time'];
-        $summary['profilingMemory'] = $data['memory'];
-        $summary['profilingNumFiles'] = $data['numFiles'];
+        // we do it this way so that the number of files remains
+        // consistent between the toolbar and the summary index data
+        // this is emulating LogTarget::export()
+        foreach ($this->module->panels as $id => $panel) {
+            $data = $panel->save();
+            if ($id == "profiling") {
+                $summary['profilingTime'] = $data['time'];
+                $summary['profilingMemory'] = $data['memory'];
+                $summary['profilingNumFiles'] = $data['numFiles'];
+                break;
+            }
+        }
 
         return $summary;
     }

1617d6a87524bcc52cd4534fd9ef185581437ced
amnah <amnah.dev@gmail.com>
2016-07-22 19:53:37 -0400

    update a few summary views

--- views/default/panels/config/summary.php
+++ views/default/panels/config/summary.php
@@ -3,7 +3,7 @@
 ?>
 <div class="yii-debug-toolbar__block">
     <a href="<?= $panel->getUrl() ?>">
-        <span class="yii-debug-toolbar__label"><?= $panel->data['application']['yii'] ?></span>
-        <span class="yii-debug-toolbar__label"><?= substr($panel->data['php']['version'], 0, 10) ?></span>
+        <span class="yii-debug-toolbar__label" title="Yii2 version"><?= $panel->data['application']['yii'] ?></span>
+        <span class="yii-debug-toolbar__label" title="PHP version"><?= substr($panel->data['php']['version'], 0, 10) ?></span>
     </a>
 </div>
--- views/default/panels/profile/summary.php
+++ views/default/panels/profile/summary.php
@@ -7,5 +7,5 @@
 <div class="yii-debug-toolbar__block">
     <a href="<?= $panel->getUrl() ?>" title="Total request processing time was <?= $time ?>"><span class="yii-debug-toolbar__label yii-debug-toolbar__label_info"><?= $time ?></span></a>
     <a href="<?= $panel->getUrl() ?>" title="Peak memory consumption"><span class="yii-debug-toolbar__label yii-debug-toolbar__label_info"><?= $memory ?></span></a>
-    <a href="<?= $panel->getUrl() ?>" title="Included files"><span class="yii-debug-toolbar__label yii-debug-toolbar__label_info"><?= $numFiles ?></span></a>
+    <a href="<?= $panel->getUrl() ?>" title="Number of included files"><span class="yii-debug-toolbar__label yii-debug-toolbar__label_info"><?= $numFiles ?></span></a>
 </div>
